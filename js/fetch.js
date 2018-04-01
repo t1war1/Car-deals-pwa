a9d949234e9596cd08f20b95a40338b8e95126f2
Gaurav <tiwarigaurav1998@gmail.com>
2018-04-02 01:10:11 +0530

    Client side storage

--- js/fetch.js
+++ js/fetch.js
@@ -1,11 +1,38 @@
+var lastItemId = null; var limit = 3;
+
 $(()=>{
+
+    window.pageEvents={
+        loadCarPage:(carId)=>{
+            $.get(`https://bstavroulakis.com/pluralsight/courses/progressive-web-apps/service/car.php?carId=${carId}`,(data)=>{
+                $('body').append(data);
+            })
+    }
+    ,
+        loadMore:()=>{
+            fetchCars((cars)=>{
+                $('#first-load').hide();
+                var cardHTML="";
+                console.log(cars.length);
+                for(var i=0;i<cars.length;i++){
+                    var temp=generateCar(cars[i]);
+                    cardHTML=cardHTML+temp;
+                }
+                console.log(cardHTML);
+                grid.append(cardHTML);
+                $('.mdl-layout__content').css({'display':'inline-block'});
+
+            });
+        }
+    }
+
     var grid=$('.mdl-grid');
     fetchCars((cars)=>{
         $('#first-load').hide();
         var cardHTML="";
         console.log(cars.length);
         for(var i=0;i<cars.length;i++){
-            var temp=generateCar(cars[i].value);
+            var temp=generateCar(cars[i]);
             cardHTML=cardHTML+temp;
         }
         console.log(cardHTML);
@@ -17,7 +44,7 @@ $(()=>{
 
 function generateCar(car) {
     var g=car.brand+" "+car.year+" "+car.price;
-    var x=`<div class="mdl-card mdl-cell mdl-cell--4-col mdl-shadow--6dp" style="background-image: url('${car.image}')">
+    var x=`<div class="mdl-card mdl-cell mdl-cell--4-col mdl-shadow--6dp" style="background-image: url('${car.image}')" onclick="pageEvents.loadCarPage('${car.details_id}')">
         <div class="mdl-card__title">
         <span class="mdl-card__title-text">${g} (${car.price})</span>
 </div>
@@ -26,10 +53,60 @@ function generateCar(car) {
 }
 
 function fetchCars(callback) {
-    $.get('http://bstavroulakis.com/pluralsight/courses/progressive-web-apps/service/latest-deals.php',
+    $.get(`http://bstavroulakis.com/pluralsight/courses/progressive-web-apps/service/latest-deals.php?carId=${getLastCarId()}`,
         (data)=>{
         console.log(data);
-            callback(data.cars);
+        addCars(data.cars).then(()=>{
+            loadMore(callback);
+        });
+
         }
         )
+}
+
+
+function loadMore(callback)  //to get cars from indexedDB after the new data is loaded into the database
+{
+    getCars().then((cars)=>{
+        callback(cars);
+    })
+}
+
+
+//clientStorage.js
+
+var carsInstance=localforage.createInstance({
+    name:"cars"
+});
+
+function addCars(newCars) //add cars to IndexedDB
+{
+    return new Promise((resolve,reject)=>{
+        carsInstance.setItems(newCars)
+            .then(()=>{
+                resolve();
+            })
+    })
+}
+
+function getCars(){  //get cars stored in IndexedDB
+    return new Promise(function(resolve, reject){
+        carsInstance.keys().then(function(keys){
+
+            var index = keys.indexOf(lastItemId);
+            if(index == -1){ index = keys.length; }
+            if(index ==  0){ resolve([]); return; }
+
+            var keys = keys.splice(index - limit, limit);
+            carsInstance.getItems(keys).then(function(results){
+                var returnArr = Object.keys(results).map(function(k) { return results[k] }).reverse();
+                lastItemId = returnArr[returnArr.length-1].id;
+                resolve(returnArr);
+            });
+        })
+    })
+}
+
+function getLastCarId(){ //to get the id of the last car loaded
+    return lastItemId;
 }
